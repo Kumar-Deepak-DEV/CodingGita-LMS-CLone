01c41371246cbb20b592078cc1562fd58454b53d
Deepak Kumar <kumar.deepak.cg.s@gmail.com>
2026-03-03 19:41:18 +0530

    create auth.js

---
+++ src/utils/auth.js
@@ -0,0 +1,27 @@
+import { students } from "./students";
+
+export const getUser = () => {
+  try {
+    const data = localStorage.getItem("user");
+    return data ? JSON.parse(data) : null;
+  } catch {
+    return null;
+  }
+};
+
+export const loginDetails = (uid, password) => {
+  const student = students.find(
+    (s) => s.uid === uid && s.password === password,
+  );
+
+  if (!student) return false;
+
+  const { password: _, ...safeUser } = student;
+
+  localStorage.setItem("user", JSON.stringify(safeUser));
+  return true;
+};
+
+export const logout = () => {
+  localStorage.removeItem("user");
+};

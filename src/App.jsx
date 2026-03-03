aa296d94b28082fbf9eb8f612fb68c8a56ebdeb0
Deepak Kumar <kumar.deepak.cg.s@gmail.com>
2026-03-03 19:40:45 +0530

    add basic details in the page

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+
+import "./App.css"
+
+function App() {
+  return (
+    <Routes>
+      
+    </Routes>
+  );
+}
+
+export default App;

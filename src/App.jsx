3212da19c3334c829f873b96567f7d783d20f14c
Deepak Kumar <kumar.deepak.cg.s@gmail.com>
2026-03-04 19:46:42 +0530

    update jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import ProtectedRoute from "./components/ProtectedRoute";
 
+import Landing from "./pages/Landing";
+
+
 
 import "./App.css"
 
 function App() {
   return (
     <Routes>
-      
+        <Route path="/" element={<Landing />} />
+        
     </Routes>
   );
 }
